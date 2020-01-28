1e26752344392fd7f997fdb3f2fb7bd5730002a8
jaganswornkar <jaganswornkar1998@gmail.com>
2020-01-29 00:46:37 +0530

    Updates

---
+++ precache-manifest.021903bf9851199c203bc9fe23e0168c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82392568c292cb55b25fcfc2a12a6474",
+    "url": "/zomato-restaurant-search/index.html"
+  },
+  {
+    "revision": "8f7799ae2c3add17febd",
+    "url": "/zomato-restaurant-search/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "39f7d217f70dccf2e1ef",
+    "url": "/zomato-restaurant-search/static/js/2.4df1bca5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/zomato-restaurant-search/static/js/2.4df1bca5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f7799ae2c3add17febd",
+    "url": "/zomato-restaurant-search/static/js/main.effa81ca.chunk.js"
+  },
+  {
+    "revision": "2ec457a3b1a9a7c81928",
+    "url": "/zomato-restaurant-search/static/js/runtime-main.e347dcf1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zomato-restaurant-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
